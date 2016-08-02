c76742d1edfad1096c0249d6076ec3fed47db9d3
DeepikaSugumar <s.deepisugu@gmail.com>
2016-08-01 22:19:05 -0700

    kjdkjjlk
    shfjhsjkfl

---
+++ hello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+	  int i;
+	  String str;
+	  System.out.println("Enter a string");
+	  Scanner s=new Scanner(System.in);
+	  str=s.nextLine();
+	  for(i=0;i<5;i++)
+	  {
+		  System.out.println("Hello");
+	  }
+	}
+
+}
